9ea641b5f4bbcf478ae2b3eaa6cb1dcdaf2d2749
joao21dev <joao21dev@gmail.com>
2022-10-01 11:25:40 -0300

    outputs added

--- 01_output_php.php
+++ 01_output_php.php
@@ -1,13 +1,24 @@
+<?php
+echo 123, '123';
+print_r('Outpdduto Word');
+print_r([1, 2, 3]);
+var_dump('Helloooo');
+var_dump(true);
+var_export('Helloooo');
+?>
+
 <!DOCTYPE html>
-<html ng-app="TestApp">
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
-<body ng-controller="testCtrl">
-    <?php
-    echo 'Outputo Word'
-    ?>
+<body>
+    <h1><?php echo 'post one'  ?></h1>
 </body>
 
 </html>
